9b2e4355995f0db6f86d123e9f2f02c81acc001f
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-17 23:53:36 +0530

    first commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
 class Carousel extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+    };
+  }
+
+  handlePrevClick = () => {
+    this.setState((prevState) => ({
+      currentIndex:
+        (prevState.currentIndex - 1 + images.length) % images.length,
+    }));
+  };
+
+  handleNextClick = () => {
+    this.setState((prevState) => ({
+      currentIndex: 
+        (prevState.currentIndex - 1 + images.length) % images.length,
+    }));
+  };
+
+  render() {
+    const { currentIndex } = this.state;
+    const currentImage = images[currentIndex];
+
+    return (
+      <div className="carousel-container">
+        <button onClick={this.handlePrevClick}>
+          <ArrowBackIosIcon />
+        </button>
+        <div className="carousel-image">
+          <img src={currentImage.img} alt="Carousel Item" />
+          <div className="carousel-text">
+            <div className="subtitle">{currentImage.subtitle}</div>
+            <div className="title">{currentImage.title}</div>
+          </div>
+        </div>
+        <button onClick={this.handleNextClick}>
+          <ArrowForwardIosIcon />
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
